2480081868983c0f681c33f6f403a4dde3df2dbe
ikhtiyaratakishiyev <ikhtiyar.atakishiyev@gmail.com>
2023-06-20 23:36:35 +0400

    Make some changes

--- src/Pages/ContactPage.jsx
+++ src/Pages/ContactPage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import Nav from '../Components/Nav/index'
+import Footer from '../Components/Footer/index'
 const ContactPage = () => {
   return (
     <div>
+      <Nav/>
         <h1>ContactPage</h1>
+        <Footer/>
     </div>
   )
 }
